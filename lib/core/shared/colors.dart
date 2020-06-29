efe9ee8d68c4e2540450e7eb5a65c197226386aa
renan mascarenhas goncalves <renanmascg@gmail.com>
2020-06-29 13:20:13 -0300

    Adiciona nova assets e cores que serão utilizadas na pagina de serviço unico do usuário

--- lib/core/shared/colors.dart
+++ lib/core/shared/colors.dart
@@ -4,6 +4,7 @@ final Color kMainGreenColor = Color(0xFF21A472);
 final Color kMainRedColor = Color(0xFFF7685B);
 final Color kMainOrangeColor = Color(0xFFFF833A);
 final Color kDividerGreyColor = Color(0xFFE5E5E5);
+final Color kBackgroundIconColor = Color(0xFFEDEEED);
 
 
 const Color kMainTextBoldColor = Color(0xFF223E5C);
