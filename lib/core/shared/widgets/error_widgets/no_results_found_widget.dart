c343a0dd06146a91a5499bf88f58cc2bd9ff56d7
renan mascarenhas goncalves <renanmascg@gmail.com>
2020-08-07 15:28:46 -0300

    Melhora a tela de no results found dentro da aplicação.

--- lib/core/shared/widgets/error_widgets/no_results_found_widget.dart
+++ lib/core/shared/widgets/error_widgets/no_results_found_widget.dart
@@ -4,6 +4,7 @@ import 'package:flutter_svg/svg.dart';
 import '../../../../features/login/presentation/widgets/main_button.dart';
 import '../../styles/colors.dart';
 import '../../styles/text_styles.dart';
+import '../sliver_main_header_widget.dart';
 
 class NoResultsFoundWidget extends StatelessWidget {
   final void Function() onPress;
@@ -12,45 +13,56 @@ class NoResultsFoundWidget extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      padding: EdgeInsets.all(30),
-      color: kMainBackground,
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.stretch,
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: <Widget>[
-          RichText(
-            textAlign: TextAlign.center,
-            text: TextSpan(
-                style: kBoldTextStyle.copyWith(
-                    fontSize: 22, fontWeight: FontWeight.normal),
-                children: [
-                  TextSpan(text: 'Que tal\n'),
-                  TextSpan(
-                      text: 'ser o primeiro?',
-                      style: TextStyle(fontWeight: FontWeight.bold))
-                ]),
-          ),
-          SvgPicture.asset(
-            'assets/nenhum_resultado.svg',
-            width: 200,
-            height: 200,
-          ),
-          Text(
-            'Sua região ainda não tem nenhum\nparceiro em nosso aplicativo!\nQue tal ser o pioneiro então?\nCadastre-se',
-            textAlign: TextAlign.center,
-            style: kBoldTextStyle.copyWith(
-              fontSize: 16,
-              fontWeight: FontWeight.normal,
+    return CustomScrollView(
+      slivers: <Widget>[
+        SliverMainHeader(title: Text('')),
+        _buildBody()
+      ],
+    );
+  }
+
+  Widget _buildBody() {
+    return SliverToBoxAdapter(
+      child: Container(
+        padding: EdgeInsets.all(30),
+        color: kMainBackground,
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            RichText(
+              textAlign: TextAlign.center,
+              text: TextSpan(
+                  style: kBoldTextStyle.copyWith(
+                      fontSize: 22, fontWeight: FontWeight.normal),
+                  children: [
+                    TextSpan(text: 'Que tal\n'),
+                    TextSpan(
+                        text: 'ser o primeiro?',
+                        style: TextStyle(fontWeight: FontWeight.bold))
+                  ]),
             ),
-          ),
-          SizedBox(height: 20),
-          MainButton(
-            color: kMainGreenColor,
-            text: 'Cadastrar',
-            onPress: onPress,
-          )
-        ],
+            SvgPicture.asset(
+              'assets/nenhum_resultado.svg',
+              width: 200,
+              height: 200,
+            ),
+            Text(
+              'Sua região ainda não tem nenhum\nparceiro em nosso aplicativo!\nQue tal ser o pioneiro então?\nCadastre-se',
+              textAlign: TextAlign.center,
+              style: kBoldTextStyle.copyWith(
+                fontSize: 16,
+                fontWeight: FontWeight.normal,
+              ),
+            ),
+            SizedBox(height: 20),
+            MainButton(
+              color: kMainGreenColor,
+              text: 'Cadastrar',
+              onPress: onPress,
+            )
+          ],
+        ),
       ),
     );
   }
